ffdc4cc12e52e2da1fab7f45f0e25ebdf32b1ce2
Martín Orue <martin.orue008@gmail.com>
2024-08-29 23:19:45 +0000

    agrego solución de Martín

--- ejercicio4.sh
+++ ejercicio4.sh
@@ -10,3 +10,15 @@ then
 else
     echo "La ruta: $PATH no existe"
 fi
+
+#$1 es el 2do param que me pasan cuando llaman al script
+if [[ -d $1 ]]; then #Llama a "test" con el param que chequea si es directorio. respectar los espacios después y antes de los paréntesis rectos
+    echo "La ruta: $ruta existe!"
+    permiso=$(ls -ld $ruta)
+    permiso=${permiso:3:1}
+    if [[ $permiso == "x" ]]; then
+        echo "Tengo permisos de ejecución sobre la ruta: $1"
+    fi
+else
+    echo "La ruta: $1 no existe!"
+fi
